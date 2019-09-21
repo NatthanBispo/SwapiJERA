89ae25b8bcf0c9729bd05e4875cb622848e92925
Natthanbispo <nathanbispo.np@gmail.com>
2019-09-21 17:20:48 -0400

    alteração na dinamica de apresentação

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
-var aux = document.createElement("div");
-function getCharacters(item){
-    console.log(item)
-    //var links = item.split(',')
-    item.forEach(criarObj);
-    divCharacters.appendChild(aux);
-}
-
-function criarObj(item, indice, array) {
-    request2 = new XMLHttpRequest();
-    console.log(item)
-    request2.open('GET', item, false);   
-    request2.send();
-    var jsonresult = JSON.parse(request2.response)
-    console.log(jsonresult.name);
-
-    
-}
-
-function getHtml(item, indice, array) {
-        /* EXEMPLO CARD
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h4 id="titulo" class="card-title"></h4>
-                <h6  id="episode_id" class="card-subtitle mb-2 text-muted"></h6>
-                <h5 id="director"></h5>
-                <h5 id="release_date"></h5>
-                <div class="dropdown show">
-                    <a class="btn btn-secondary dropdown-toggle dropdownMenuLink" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Characters
-                    </a>
-                    <div id="characters" class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <!--Itens-->
-                    </div>
-                </div>
-
-                <h5 id="planets"></h5>
-                <h5 id="starships"></h5>
-                <h5 id="vehicles"></h5>
-                <h5 id="species"></h5>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
-        </div>*/
+function getNames(item, destino){
+    item = item.split(",");
+    var lista = document.createElement("div");
+    for (var i = 0; i < item.length; i++) {
+        request2 = new XMLHttpRequest();
+        request2.open('GET', item[i], false);   
+        request2.send();
+        var jsonresult = JSON.parse(request2.response)
+        console.log(jsonresult.name);
         
+        var da = document.createElement("h5");
+            da.appendChild(document.createTextNode(jsonresult.name));
+        lista.appendChild(da);
+    }
+    document.querySelector("#"+destino).appendChild(lista);
+}
+
+function getHtml(item, indice, array) {        
     var divCard = document.createElement("div");
+        divCard.setAttribute('id', 'card'+indice+'');
         divCard.setAttribute('class', 'card');
         divCard.setAttribute('style', 'width: 18rem;');
     
@@ -51,23 +25,22 @@ function getHtml(item, indice, array) {
         divCardBody.setAttribute('class', 'card-body');
 
     var idTitulo = document.createElement("h4");
-        idTitulo.setAttribute('id', 'titulo');
+        idTitulo.setAttribute('id', 'titulo'+indice+'');
         idTitulo.setAttribute('class', 'card-title');
         idTitulo.appendChild(document.createTextNode(item.title));
 
     var episode_id = document.createElement("h6");
-        episode_id.setAttribute('id', 'episode_id');
+        episode_id.setAttribute('id', 'episode_id'+indice+'');
         episode_id.setAttribute('class', 'card-subtitle mb-2 text-muted');
         episode_id.appendChild(document.createTextNode(item.episode_id));
 
     var director = document.createElement("h5");
-        director.setAttribute('id', 'director');
+        director.setAttribute('id', 'director'+indice+'');
         director.appendChild(document.createTextNode(item.director));
 
     var release_date = document.createElement("h5");
-        release_date.setAttribute('id', 'release_date');
+        release_date.setAttribute('id', 'release_date'+indice+'');
         release_date.appendChild(document.createTextNode(item.release_date));
-        
 
     // Dropdown Characters
     var divDropdownCharacters = document.createElement("div");
@@ -76,19 +49,17 @@ function getHtml(item, indice, array) {
     var aBtnCharacters = document.createElement("a");
         aBtnCharacters.setAttribute('class', 'btn btn-secondary dropdown-toggle'); 
         aBtnCharacters.setAttribute('role', 'button');
-        aBtnCharacters.setAttribute('id', 'dropdownMenuLink');
+        aBtnCharacters.setAttribute('id', 'dropdownMenuLink'+indice+'');
         aBtnCharacters.setAttribute('data-toggle', 'dropdown');
         aBtnCharacters.setAttribute('aria-haspopup', 'true');
         aBtnCharacters.setAttribute('aria-expanded', 'false');
-        getCharacters(item.characters)
-        //aBtnCharacters.setAttribute('onclick', "getCharacters('"+item.characters+"')");
+        aBtnCharacters.setAttribute('onclick', 'getNames("'+item.characters+'", "characters'+indice+'")');
         aBtnCharacters.appendChild(document.createTextNode("Characters"));
         
     var divCharacters = document.createElement("div");
-        divCharacters.setAttribute('id', 'characters'); 
+        divCharacters.setAttribute('id', 'characters'+indice+''); 
         divCharacters.setAttribute('class', 'dropdown-menu');   
-        divCharacters.setAttribute('aria-labelledby', 'dropdownMenuLink'); 
-        
+        divCharacters.setAttribute('aria-labelledby', 'dropdownMenuLink');  
 
     // Dropdown Planets
     var divDropdownPlanets = document.createElement("div");
@@ -97,14 +68,15 @@ function getHtml(item, indice, array) {
     var aBtnPlanets = document.createElement("a");
         aBtnPlanets.setAttribute('class', 'btn btn-secondary dropdown-toggle');
         aBtnPlanets.setAttribute('role', 'button');
-        aBtnPlanets.setAttribute('id', 'dropdownMenuLink');
+        aBtnPlanets.setAttribute('id', 'dropdownMenuLink'+indice+'');
         aBtnPlanets.setAttribute('data-toggle', 'dropdown');
         aBtnPlanets.setAttribute('aria-haspopup', 'true');
         aBtnPlanets.setAttribute('aria-expanded', 'false');
+        aBtnPlanets.setAttribute('onclick', 'getNames("'+item.planets+'", "planets'+indice+'")');
         aBtnPlanets.appendChild(document.createTextNode("Planets"));
         
     var divPlanets = document.createElement("div");
-        divPlanets.setAttribute('id', 'characters'); 
+        divPlanets.setAttribute('id', 'planets'+indice+''); 
         divPlanets.setAttribute('class', 'dropdown-menu');   
         divPlanets.setAttribute('aria-labelledby', 'dropdownMenuLink');
     
@@ -115,14 +87,15 @@ function getHtml(item, indice, array) {
     var aBtnStarships = document.createElement("a");
         aBtnStarships.setAttribute('class', 'btn btn-secondary dropdown-toggle');
         aBtnStarships.setAttribute('role', 'button');
-        aBtnStarships.setAttribute('id', 'dropdownMenuLink');
+        aBtnStarships.setAttribute('id', 'dropdownMenuLink'+indice+'');
         aBtnStarships.setAttribute('data-toggle', 'dropdown');
         aBtnStarships.setAttribute('aria-haspopup', 'true');
         aBtnStarships.setAttribute('aria-expanded', 'false');
+        aBtnStarships.setAttribute('onclick', 'getNames("'+item.starships+'", "starships'+indice+'")');
         aBtnStarships.appendChild(document.createTextNode("Starships"));
         
     var divStarships = document.createElement("div");
-        divStarships.setAttribute('id', 'characters'); 
+        divStarships.setAttribute('id', 'starships'+indice+''); 
         divStarships.setAttribute('class', 'dropdown-menu');   
         divStarships.setAttribute('aria-labelledby', 'dropdownMenuLink');
 
@@ -133,14 +106,15 @@ function getHtml(item, indice, array) {
     var aBtnVehicles = document.createElement("a");
         aBtnVehicles.setAttribute('class', 'btn btn-secondary dropdown-toggle');
         aBtnVehicles.setAttribute('role', 'button');
-        aBtnVehicles.setAttribute('id', 'dropdownMenuLink');
+        aBtnVehicles.setAttribute('id', 'dropdownMenuLink'+indice+'');
         aBtnVehicles.setAttribute('data-toggle', 'dropdown');
         aBtnVehicles.setAttribute('aria-haspopup', 'true');
         aBtnVehicles.setAttribute('aria-expanded', 'false');
+        aBtnVehicles.setAttribute('onclick', 'getNames("'+item.vehicles+'", "vehicles'+indice+'")');
         aBtnVehicles.appendChild(document.createTextNode("Vehicles"));
         
     var divVehicles = document.createElement("div");
-        divVehicles.setAttribute('id', 'characters'); 
+        divVehicles.setAttribute('id', 'vehicles'+indice+''); 
         divVehicles.setAttribute('class', 'dropdown-menu');   
         divVehicles.setAttribute('aria-labelledby', 'dropdownMenuLink');
 
@@ -151,14 +125,15 @@ function getHtml(item, indice, array) {
     var aBtnSpecies = document.createElement("a");
         aBtnSpecies.setAttribute('class', 'btn btn-secondary dropdown-toggle');
         aBtnSpecies.setAttribute('role', 'button');
-        aBtnSpecies.setAttribute('id', 'dropdownMenuLink');
+        aBtnSpecies.setAttribute('id', 'dropdownMenuLink'+indice+'');
         aBtnSpecies.setAttribute('data-toggle', 'dropdown');
         aBtnSpecies.setAttribute('aria-haspopup', 'true');
         aBtnSpecies.setAttribute('aria-expanded', 'false');
+        aBtnSpecies.setAttribute('onclick', 'getNames("'+item.species+'", "species'+indice+'")');
         aBtnSpecies.appendChild(document.createTextNode("Species"));
         
     var divSpecies = document.createElement("div");
-        divSpecies.setAttribute('id', 'characters'); 
+        divSpecies.setAttribute('id', 'species'+indice+''); 
         divSpecies.setAttribute('class', 'dropdown-menu');   
         divSpecies.setAttribute('aria-labelledby', 'dropdownMenuLink');
     
@@ -194,20 +169,20 @@ function getHtml(item, indice, array) {
 
 var request = new XMLHttpRequest();
 var vetor;
-var arrayFilmes;
 
-request.open('GET', 'https://swapi.co/api/films', true);
+request.open('GET', 'https://swapi.co/api/films', false);
 request.send();
-console.log("open")
+vetor = (JSON.parse(request.response)).results;
+vetor.forEach(getHtml)
 var t = false;
-
+/*
 request.onload = function () {
     vetor = (JSON.parse(request.response)).results;
-    console.log("json") 
-    if(t == false)
-        vetor.forEach(getHtml);
-        else
-            t = true;
+    console.log("json")
+    if(t == false){
+        vetor.forEach(getHtml)
+        t = true;
+    }
 }
 /*
 function criarObjs(item, indice, array){
